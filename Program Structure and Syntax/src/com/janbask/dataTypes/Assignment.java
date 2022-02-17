2be6bf5c16890d9d87c3ac7806a10ed026b4bbf1
Som <somaiah1988@gmail.com>
2022-02-16 21:58:51 -0500

    16

--- Program Structure and Syntax/src/com/janbask/dataTypes/Assignment.java
+++ Program Structure and Syntax/src/com/janbask/dataTypes/Assignment.java
@@ -4,6 +4,22 @@ public class Assignment {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		String tool="selenium";
+		
+		double version=4.12;
+		
+		String program="Java";
+		
+		String program1="python";
+		
+		String browser="All browser";
+		
+		// Selenium is a open source tool
+		//Selenium supports java programming language
+		//Selenium latest version is 4.12 and it supports java and python language and also supports All browser
+		// Selenium with Java is widely used with latest version 4.12 
+		
 
 	}
 
